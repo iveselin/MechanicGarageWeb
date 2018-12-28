ec01fd8916a3d368778afca25c81b46ca017e207
Ivan Veselin <ivanveselin@hotmail.com>
2018-12-28 18:39:44 +0100

    moved all the logic to controllers

--- controllers/administrationController.js
+++ controllers/administrationController.js
@@ -2,10 +2,65 @@ const firebaseAdmin = require('firebase-admin');
 
 exports.auth_check = (req, res, next) => {
     if (req.isAuthenticated()) {
-        next();
+        return next();
     } else {
         //req.flash('error_msg','You are not logged in');
         res.redirect('../login');
     }
 };
 
+exports.admin_index = (req, res) => {
+    res.render('admin/admin', { title: 'Restricted Garage access', user: req.user });
+}
+
+exports.requests_get = (req, res, next) => {
+    var db = firebaseAdmin.firestore();
+    var requestsReference = db.collection('requests');
+    var data = [];
+
+    requestsReference.get()
+        .then((querySnapshot) => {
+            if (querySnapshot.empty) {
+                console.log('No data');
+            } else {
+                querySnapshot.docs.map(doc => {
+                    var dataobject = doc.data();
+                    dataobject.object_id = doc.id;
+                    data.push(dataobject);
+                });
+            }
+            res.render('admin/requests', {
+                user: req.user,
+                data: data,
+                title: 'Zahtjevi'
+            });
+        })
+
+        .catch((error) => {
+            console.log(error);
+            return next(error);
+        });
+}
+
+exports.request_by_Id_get = (req, res, next) => {
+    var db = firebaseAdmin.firestore();
+    var requestsReference = db.collection('requests');
+
+    requestsReference.doc(req.params.id).get()
+        .then(doc => {
+            if (!doc.exists) {
+                console.log('No such document!');
+                return next(new Error('No such document'));
+            } else {
+                res.render('admin/request_details', {
+                    user: req.user,
+                    data: doc.data(),
+                    title: 'Zahtjev ' + req.params.id
+                });
+            }
+        })
+        .catch(err => {
+            console.log('Error getting document', err);
+            return next(err);
+        });
+}
--- routes/admin.js
+++ routes/admin.js
@@ -7,9 +7,7 @@ var administration_controller = require('../controllers/administrationController
 //auth checking middleware
 router.use(administration_controller.auth_check);
 
-router.get('/', (req, res) => {
-  res.render('admin/admin', { title: 'Restricted Garage access', user: req.user });
-});
+router.get('/', administration_controller.admin_index);
 
 //to be continued
 router.get('/workers', (req, res) => {
@@ -17,57 +15,10 @@ router.get('/workers', (req, res) => {
 });
 
 //get all pending requests(add pending part)
-router.get('/requests', (req, res) => {
-  var db = firebaseAdmin.firestore();
-  var requestsReference = db.collection('requests');
-
-  requestsReference.get()
-    .then((querySnapshot) => {
-      if (querySnapshot.empty) {
-        console.log('No data');
-      } else {
-        var data = [];
-        querySnapshot.docs.map(doc => {
-          var dataobject = doc.data();
-          dataobject.object_id = doc.id;
-          data.push(dataobject);
-        });
-
-        res.render('admin/requests', {
-          user: req.user,
-          data: data,
-          title: 'Zahtjevi'
-        });
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+router.get('/requests', administration_controller.requests_get);
 
 
 //gets the data of clicked job_request by position, try to find doc Id and then query the data
-router.get('/requests/:id', (req, res, next) => {
-  var db = firebaseAdmin.firestore();
-  var requestsReference = db.collection('requests');
-
-  requestsReference.doc(req.params.id).get()
-    .then(doc => {
-      if (!doc.exists) {
-        console.log('No such document!');
-        return next(new Error('No such document'));
-      } else {
-        res.render('admin/request_details', {
-          user: req.user,
-          data: doc.data(),
-          title: 'Zahtjev ' + req.params.id
-        });
-      }
-    })
-    .catch(err => {
-      console.log('Error getting document', err);
-      return next(err);
-    });
-});
+router.get('/requests/:id', administration_controller.request_by_Id_get);
 
 module.exports = router;
